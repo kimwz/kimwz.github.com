7138648f7f98f2775b0f8fb807edab31763277fc
kimwz <kimwz@zuminternet.com>
2013-09-17 04:28:43 +0000

    modified xss.js

--- xss.js
+++ xss.js
@@ -1,1 +1,16 @@
-$(function(){ var $rota = $(".btn"), $reverse=$("#textarea_content");$reverse.css({"background-color":"yellow","transform":"rotate(180deg)"}); $(function() {var degree = 0,timer;function rotate() { $rota.css({"z-index":10000, transform: "scale(" (1.0 ((degree/100)%0.5)) ") rotate(" degree "deg)"}); timer = setTimeout(function() { degree = 2; rotate(); },10); } rotate(); }); })
+function xss(){ 
+	var $rota = $(".btn");
+	var $reverse=$("#textarea_content");
+	$reverse.css({"background-color":"yellow","transform":"rotate(180deg)"}); 
+	$(function() {
+		var degree = 0,timer;
+		function rotate() { 
+			$rota.css({"z-index":10000, transform: "scale(" (1.0 ((degree/100)%0.5)) ") rotate(" degree "deg)"}); 
+			timer = setTimeout(function() { 
+				degree = 2; rotate(); 
+			},10); 
+		} 
+		rotate(); 
+	}); 
+};
+xss();
