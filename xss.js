52a216c79278d9b2bf2d4c0dc2653e415c2b0963
kimwz <kimwz@zuminternet.com>
2013-09-17 04:22:01 +0000

    added xss.js

---
+++ xss.js
@@ -0,0 +1,1 @@
+$(function(){ var $rota = $(".btn"), $reverse=$("#textarea_content");$reverse.css({"background-color":"yellow","transform":"rotate(180deg)"}); $(function() {var degree = 0,timer;function rotate() { $rota.css({"z-index":10000, transform: "scale(" (1.0 ((degree/100)%0.5)) ") rotate(" degree "deg)"}); timer = setTimeout(function() { degree = 2; rotate(); },10); } rotate(); }); })
